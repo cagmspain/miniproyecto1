0282d6f879fb15f9d78c5e8a7a6a3dca74be2953
cagmspain <cagmspain@gmail.com>
2022-06-26 22:34:51 +0200

    adding panels and improving JS code

--- JS/bateria.js
+++ JS/bateria.js
@@ -12,8 +12,53 @@ const sounds = {
   H: "./sounds/tom-mid.wav",
 };
 
-//selecciono section drumpad
-const drumpadSection = document.querySelector("#drumpad");
+//selecciono y defino secciones
+const initialSection = document.querySelector("section.initial");
+console.log(initialSection);
+const drumpadSection = document.querySelector("section.play");
+console.log(drumpadSection);
+const playRecSection = document.querySelector("section.playRec");
+console.log(playRecSection);
+
+function showPanel(panel) {
+  panel.classList.remove("hidden");
+}
+function hideAllPanel() {
+  initialSection.classList.add("hidden");
+  drumpadSection.classList.add("hidden");
+  playRecSection.classList.add("hidden");
+}
+
+function ShowPlayRec() {
+  showPanel(playRecSection);
+  const returnPlayButton = playRecSection.querySelector("button");
+  returnPlayButton.addEventListener("click", () => {
+    hideAllPanel();
+    showCentral();
+  });
+}
+function showCentral() {
+  showPanel(drumpadSection);
+  const recordingButton = drumpadSection.querySelector("button.rec");
+
+  recordingButton.addEventListener("click", (e) => {
+    hideAllPanel();
+    ShowPlayRec();
+  });
+}
+
+function managePanel() {
+  showPanel(initialSection);
+  const letsPlayButton = initialSection.querySelector("button");
+
+  letsPlayButton.addEventListener("click", () => {
+    hideAllPanel();
+    showCentral();
+  });
+}
+
+managePanel();
+
 //función play sounds
 const drumPadPlay = (pathSound) => {
   const sound = new Audio(pathSound);
@@ -28,13 +73,18 @@ let recordingArray = [];
 
 const UserClick = drumpadSection.addEventListener("click", (event) => {
   const target = event.target;
-  if (target.matches("#drumpad>button")) {
+  if (target.matches("section.play>button")) {
     let soundName = target.getAttribute("id");
-    setTimeout(() => {
-      drumPadPlay(`./sounds/${soundName}.wav`);
-    }, 100);
+    if (soundName !== null) {
+      setTimeout(() => {
+        drumPadPlay(`./sounds/${soundName}.wav`);
+        //recordingArray.push(`./sounds/${soundName}.wav`);
+      }, 100);
+    }
+    saveLocalStorage(recordingArray);
+    let arrS = getLocalStorage();
+    console.log(`Arr: ${arrS}`);
     //console.log(`./sounds/${soundName}.wav`);
-    recordingArray.push(`./sounds/${soundName}.wav`);
   }
 });
 
@@ -49,12 +99,26 @@ const UserKey = drumpadSection.addEventListener("keydown", (event) => {
       drumPadPlay(sounds[key]);
       //recordingArray.push(sounds[key]);
     }, 100);
+    //saveLocalStorage(recordingArray);
+    let arrS = getLocalStorage();
+    console.log(`Arr: ${arrS}`);
   } else {
     alert(`pulsa una tecla valida "E" "R" "T" "Y" "U" "I" "F" "G" "H"`);
   }
 });
 
-console.log(recordingArray);
-
 //testing local storage
-window.localStorage.setItem("rec", JSON.stringify(recordingArray));
+
+function saveLocalStorage(arrSounds) {
+  window.localStorage.setItem("rec", JSON.stringify(arrSounds));
+}
+
+function getLocalStorage() {
+  return [...JSON.parse(window.localStorage.getItem("rec"))];
+}
+
+//1)Hacer un eventListener para el boton REC donde guarde el array de sonidos
+//en el localStorage
+
+//2)Hacer un boton de play con un eventListener para recoger el array de
+//sonidos del LocalStorage y reproducirlo
